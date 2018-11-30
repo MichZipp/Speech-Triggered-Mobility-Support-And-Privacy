194f4e30f156260e3c08708c97ed7d5b55a2381d
Michael <Michael@Zipperle.de>
2018-11-30 14:08:09 +0100

    lambda function set up!

---
+++ Prototyp/Bot/README.md
@@ -0,0 +1,4 @@
+# Mobility Support Bot
+## Commands
+- list lambda functions: `aws lambda list-functions`
+- upload function to lambda `./publish.sh`
---
+++ Prototyp/Bot/lambda/index.js
@@ -0,0 +1,35 @@
+ // Close dialog with the customer, reporting fulfillmentState of Failed or Fulfilled ("Thanks, your pizza will arrive in 20 minutes")
+ function close(sessionAttributes, fulfillmentState, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+        },
+    };
+}
+
+// --------------- Events ----------------------- 
+function dispatch(intentRequest, callback) {
+    console.log('request received for userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.intentName}');
+    const sessionAttributes = intentRequest.sessionAttributes;
+    const intentName = intentRequest.currentIntent;
+    
+    console.log("Intent: " + intentName);
+    console.log("Session: " + sessionAttributes);
+    
+    callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `Test`}));
+}
+
+// --------------- Main handler ----------------------
+exports.handler = (event, context, callback) => {
+    try {
+        dispatch(event,
+            (response) => {
+                callback(null, response);
+            });
+    } catch (err) {
+        callback(err);
+    }
+};
