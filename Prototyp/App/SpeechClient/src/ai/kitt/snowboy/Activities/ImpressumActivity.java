1c4b271b9c94183d0d09b9c347e30feae3cf9905
jennybelov <jenni.belov@t-online.de>
2018-12-09 15:07:18 +0100

    Test Commit

--- Prototyp/App/SpeechClient/src/ai/kitt/snowboy/Activities/ImpressumActivity.java
+++ Prototyp/App/SpeechClient/src/ai/kitt/snowboy/Activities/ImpressumActivity.java
@@ -2,6 +2,9 @@ package ai.kitt.snowboy.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import ai.kitt.snowboy.demo.R;
 
@@ -11,6 +14,11 @@ public class ImpressumActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_impressum);
+
+        TextView textView = ( TextView)findViewById(R.id.textView);
+
+        TextView linkView = (TextView)findViewById(R.id.linkView);
+        linkView.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 }
